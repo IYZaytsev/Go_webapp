04505a8f1df6aee9b02e15f7d66b0c3aa2b9eee9
Ivan <IvanZaytsev123@gmail.com>
2019-09-23 14:06:38 -0400

    added remove and add methods

--- studentTeacherDatabase.go
+++ studentTeacherDatabase.go
@@ -7,7 +7,9 @@ type student struct {
 }
 
 type class struct {
-	students []student
+	students  []student
+	id        int
+	teacherID int
 }
 
 type teacher struct {
@@ -17,8 +19,8 @@ type teacher struct {
 
 func main() {
 	dataBaseStudent := make([]student, 0)
-	//dataBaseClass := make([]class, 0)
-	//dataBaseTeacher := make([]teacher, 0)
+	dataBaseClass := make([]class, 0)
+	dataBaseTeacher := make([]teacher, 0)
 	//fmt.Println(dataBaseStudent)
 	//fmt.Println(dataBaseClass)
 	//fmt.Println(dataBaseTeacher)
@@ -26,17 +28,17 @@ func main() {
 	fmt.Println("adding to student")
 	addStudent(29, &dataBaseStudent)
 	fmt.Println("student added", dataBaseStudent)
-	deleteStudent(29, &dataBaseStudent)
-	fmt.Println("student deleted", dataBaseStudent)
+	//deleteStudent(29, &dataBaseStudent)
+	//fmt.Println("student deleted", dataBaseStudent)
+	addClass(&dataBaseClass, dataBaseStudent, 1, 2)
+	addTeacher(2, dataBaseClass, &dataBaseTeacher)
 }
 
 func addStudent(id int, db *[]student) {
 	realValue := *db
-	fmt.Println(realValue)
 	*db = append(realValue, student{id})
-
-	//fmt.Println(realValue)
 }
+
 func deleteStudent(taggedforRemoval int, db *[]student) {
 	realValue := *db
 	tempSlice := make([]student, 0)
@@ -49,18 +51,29 @@ func deleteStudent(taggedforRemoval int, db *[]student) {
 	}
 	*db = tempSlice
 }
-func addTeacher(id int, db *[]student) {
-	realValue := *db
-	fmt.Println(realValue)
-	*db = append(realValue, student{id})
 
+func addTeacher(teacherID int, classes []class, db *[]teacher) {
+	realValue := *db
+	*db = append(realValue, teacher{teacherID, classes})
 	//fmt.Println(realValue)
 }
 
-func addClass(id int, db *[]student) {
+func deleteTeacher(taggedforRemoval int, db *[]teacher) {
+	realValue := *db
+	tempSlice := make([]teacher, 0)
+	for i := 0; i < len(realValue); i++ {
+
+		if realValue[i].id == taggedforRemoval {
+			continue
+		}
+		tempSlice = append(tempSlice, teacher{realValue[i].id, realValue[i].classes})
+	}
+	*db = tempSlice
+}
+func addClass(db *[]class, students []student, id int, teacherID int) {
 	realValue := *db
 	fmt.Println(realValue)
-	*db = append(realValue, student{id})
+	*db = append(realValue, class{students, id, teacherID})
 
 	//fmt.Println(realValue)
 }
