0d92692c26699ad1352b49f94434bf0dc466f33d
Ivan <IvanZaytsev123@gmail.com>
2019-09-30 21:20:17 -0400

    added cs/html and some basic HTTP handling

--- studentTeacherDatabase.go
+++ studentTeacherDatabase.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 
@@ -11,11 +12,34 @@ import (
 var dataBaseStudent []dataBaseTypes.Student
 var dataBaseClass []dataBaseTypes.Class
 var dataBaseTeacher []dataBaseTypes.Teacher
+var tmpls = template.Must(template.ParseFiles("tmpl/index.html", "tmpl/create.html"))
 
+type Page struct {
+	Title  string
+	Header string
+}
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+
+	data := Page{Title: "School Database", Header: "What type would you like to create?"}
+	if err := tmpls.ExecuteTemplate(w, "create.html", data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	data := Page{Title: "School Database", Header: "Welcome, please select an option"}
+	if err := tmpls.ExecuteTemplate(w, "index.html", data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 func main() {
-	fs := http.FileServer(http.Dir("statics"))
-	http.Handle("/", fs)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/create/", createHandler)
 
-	log.Println("Listening...")
-	http.ListenAndServe(":3000", nil)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	log.Println("listening")
+	log.Fatalln(http.ListenAndServe(":9000", nil))
 }
