cb95026a77f71c4ae3c15ba0879bf3db38d64c58
Ivan <IvanZaytsev123@gmail.com>
2019-10-15 22:27:02 -0400

    finished delete function

--- handlefunc/deleteFunc.go
+++ handlefunc/deleteFunc.go
@@ -2,7 +2,6 @@ package handlefunc
 
 import (
 	"net/http"
-	"strconv"
 	"strings"
 )
 
@@ -17,28 +16,12 @@ func DeleteHandler(w http.ResponseWriter, r *http.Request) {
 	switch databaseType {
 	case "student":
 		APICallStudent(w, r, "/students/delete", "DELETE", ID)
-		//http.Redirect(w, r, "/search/result/student", http.StatusSeeOther)
+		http.Redirect(w, r, "/search/result/student", http.StatusSeeOther)
 	case "teacher":
-		_, classList := APICallClass(w, r, "/classes/search", "GET", "//")
-		_, teacherList := APICallTeacher(w, r, "/teachers/search", "GET", "//"+ID)
-
-		selectString := "<h1> Editing " + teacherList[0].Name + "</h1> Assign Class<select name = 'class_list'>"
-		selectString += "<option value = none>none</option>"
-		for i := range classList {
-			selectString += "<option value=" + classList[i].Name + ">" + classList[i].Name + "</option>"
-		}
-		selectString += "</select> <div>Teacher: <textarea name='teacherName' rows='1' cols='15'>" + teacherList[0].Name + "</textarea></div>"
-		selectString += "<input type='hidden' id='ID' name='ID' value=" + strconv.Itoa(teacherList[0].ID) + ">"
-		data := Page{Title: "Edit Page", Content: selectString, Teacher: true, Type: "teacher"}
-		TemplateInit(w, "update.html", data)
-
+		APICallStudent(w, r, "/teachers/delete", "DELETE", ID)
+		http.Redirect(w, r, "/search/result/teacher", http.StatusSeeOther)
 	case "class":
-		_, classList := APICallClass(w, r, "/classes/search", "GET", "//"+ID)
-
-		selectString := "<h1> Editing " + classList[0].Name + "</h1>"
-		selectString += "<div>ClassName: <textarea name='className' rows='1' cols='15'>" + classList[0].Name + "</textarea></div>"
-		selectString += "<input type='hidden' id='ID' name='ID' value=" + strconv.Itoa(classList[0].ID) + ">"
-		data := Page{Title: "Edit Page", Content: selectString, Class: true, Type: "class"}
-		TemplateInit(w, "update.html", data)
+		APICallStudent(w, r, "/classes/delete", "DELETE", ID)
+		http.Redirect(w, r, "/search/result/class", http.StatusSeeOther)
 	}
 }
